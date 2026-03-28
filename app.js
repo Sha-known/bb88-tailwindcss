b0e5ca190f2d77e60b1f5c1030e7bba8fe6fea27
Sha-known <shanong.ticse@gmail.com>
2026-03-28 10:20:17 +0800

    Hydrated the footer and fix some call in app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import { loadNav } from './src/js/sections/nav.js';
 import { loadHome } from './src/js/sections/home.js';
 import { loadServices } from './src/js/sections/services.js';
+import {loadTeam} from './src/js/sections/team.js';
+import {loadPosts} from './src/js/sections/posts.js';
+import { loadFooter } from "./src/js/sections/footer.js";
 
 loadNav();
 loadHome();
 loadServices();
+loadFooter();
+loadTeam();
+loadPosts();
