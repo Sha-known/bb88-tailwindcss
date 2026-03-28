2199706d7cf3ca9b87a3bd2fecf9303eed91fba5
Sha-known <shanong.ticse@gmail.com>
2026-03-28 10:22:33 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { loadNav } from './src/js/sections/nav.js';
 import { loadHome } from './src/js/sections/home.js';
 import { loadServices } from './src/js/sections/services.js';
+import {loadPartners} from './src/js/sections/partner.js';
 import {loadTeam} from './src/js/sections/team.js';
 import {loadPosts} from './src/js/sections/posts.js';
 import { loadFooter } from "./src/js/sections/footer.js";
@@ -8,6 +9,7 @@ import { loadFooter } from "./src/js/sections/footer.js";
 loadNav();
 loadHome();
 loadServices();
+loadPartners();
 loadFooter();
 loadTeam();
 loadPosts();
