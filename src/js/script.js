b42dd14cd940631ebd2598e777aa9d76388bb54c
Sha-known <shanong.ticse@gmail.com>
2026-04-01 10:00:59 +0800

    Added back to top button since the navbar is being hidden once you scrolled down

--- src/js/script.js
+++ src/js/script.js
@@ -142,3 +142,23 @@ export function initCardHover() {
     });
   });
 }
+
+const backToTopBtn = document.getElementById('backToTop');
+
+if (backToTopBtn) {
+  window.addEventListener('scroll', () => {
+    // Show button if scrolled more than 300px from the top
+    if (window.scrollY > 300) {
+      backToTopBtn.classList.add('visible');
+    } else {
+      backToTopBtn.classList.remove('visible');
+    }
+  });
+
+  backToTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+}
